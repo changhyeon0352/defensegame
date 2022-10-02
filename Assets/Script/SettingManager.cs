f6ba42eff30bc6b1812a259b8edd1c75444736cb
ChangHyoen <dpdlfhf@gmail.com>
2022-10-03 00:15:31 +0900

    세팅시 셰이더 변경 작업중 임시

--- Assets/Script/SettingManager.cs
+++ Assets/Script/SettingManager.cs
@@ -31,6 +31,7 @@ public class SettingManager : MonoBehaviour
    
     private void Update()
     {
+        //세팅 시작 버튼
         if(Input.GetKeyDown(KeyCode.Alpha1))
         {
             if (unitSetList.Count==0)
@@ -39,15 +40,16 @@ public class SettingManager : MonoBehaviour
                 obj.name = "unitGroup";
                 unitGroupTr = obj.transform;
                 unitGroupTr.parent = transform;
+                
 
                 AddUnitToList();
             }
         }
         
-        
+        //세팅중
         if(unitSetList.Count>0)
         {
-
+            //휠 변경
             if(Input.GetAxis("Mouse ScrollWheel")>0)
             {
                 AddUnitToList();
@@ -60,7 +62,7 @@ public class SettingManager : MonoBehaviour
                 }
             }
             
-
+            // 유닛그룹 회전
             if (Input.GetKey(KeyCode.Q))
             {
                 unitGroupTr.rotation *= Quaternion.Euler(0f, -360f * Time.deltaTime, 0f);
@@ -69,9 +71,10 @@ public class SettingManager : MonoBehaviour
             {
                 unitGroupTr.rotation *= Quaternion.Euler(0f, 360f * Time.deltaTime, 0f);
             }
-
+            //유닛세팅 완료
             if (Input.GetMouseButtonDown(0))
             {
+                //ShaderChange(1);
                 unitGroupTr.parent = transform.parent;
                 unitSetList.Clear();
                 unitGroupTr = null;
@@ -79,8 +82,8 @@ public class SettingManager : MonoBehaviour
                 //transform.rotation = Quaternion.identity;
             }
 
+            //유닛그룹 정렬시키기
             float centerDiff = 0;
-
             if(unitSetList.Count%2==0)
             {
                 centerDiff=unitSetList.Count/2-0.5f;
@@ -89,8 +92,6 @@ public class SettingManager : MonoBehaviour
             {
                 centerDiff=(unitSetList.Count-1)/2;
             }
-
-
             for (int i = 0; i < unitSetList.Count; i++)
             {
                 //짝수면 n/2-0.5  홀수면 (n-1)/2 
@@ -98,14 +99,11 @@ public class SettingManager : MonoBehaviour
                 ray.origin += unitOffset*(i-centerDiff)*unitGroupTr.right ;
                 if (Physics.Raycast(ray, out RaycastHit hit, 1000.0f, LayerMask.GetMask("Ground")))
                 {
-
                     unitSetList[i].transform.position = hit.point;
-                    //리스트에 따라 
                 }
-
             }
         }
-        else
+        else // 유닛 세팅 고치기
         {
             if (Input.GetMouseButtonDown(0))
             {
@@ -114,12 +112,14 @@ public class SettingManager : MonoBehaviour
                 {
                     unitGroupTr = hit.transform.parent;
                     unitGroupTr.parent = transform;
-                    //transform.rotation = unitGroupTr.rotation;
                     for(int i = 0; i < unitGroupTr.childCount; i++)
                     {
                         unitSetList.Add(unitGroupTr.GetChild(i).gameObject);
                     }
-                    //리스트에 따라 
+                }
+                if(unitGroupTr!=null&&unitGroupTr.childCount==unitSetList.Count)
+                {
+                    //ShaderChange(0);
                 }
             }
         }
@@ -134,5 +134,15 @@ public class SettingManager : MonoBehaviour
         GameObject obj = unitSetList[unitSetList.Count - 1];
         unitSetList.Remove(obj);
         Destroy(obj);
+    }
+    void ShaderChange(int isSpawning1or0)
+    {
+        SkinnedMeshRenderer[] skinRen = unitGroupTr.GetComponentsInChildren<SkinnedMeshRenderer>();
+
+
+        for (int i = 0; i < skinRen.Length; i++)
+        {
+            skinRen[i].material.SetInt("_IsSpawning", isSpawning1or0);
+        }
     }
 }
