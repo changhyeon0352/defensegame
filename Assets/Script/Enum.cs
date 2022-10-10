6c63f9783625fe7c32a611a3623ed611403c1662
ChangHyoen <dpdlfhf@gmail.com>
2022-10-10 17:55:39 +0900

    유닛 유한상태머신 중간저장

---
+++ Assets/Script/Enum.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum unitState
+{
+    Idle=0,
+    Move,
+    Chase,
+    Attack,
+    Dead
+}
--- Assets/Script/Unit.cs
+++ Assets/Script/Unit.cs
@@ -1,18 +1,110 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Unit : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    NavMeshAgent agent;
+    Transform targetTr;
+    Animator anim;
+    unitState state = unitState.Idle;
+    private void Awake()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        anim= GetComponent<Animator>();
+    }
+    private void Update()
+    {
+        if (targetTr != null)
+            agent.SetDestination(targetTr.position);
+
+        switch (state)
+        {
+            case unitState.Idle:
+                IdleUpdate();
+                break;
+            case unitState.Move:
+                MoveUpdate();
+                break;
+            case unitState.Chase:
+                ChaseUpdate();
+                break;
+            case unitState.Attack:
+                AttackUpdate();
+                break;
+            case unitState.Dead:
+            default:
+                break;
+        }
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void IdleUpdate()
     {
-        
+        // 아군유닛 대기상태
+        // 아군유닛 이동으로 전환
+        //궁수는  Attack 전환
+        //전사는  Chase 전환
+
+    }
+    private void MoveUpdate()
+    {
+        //적 chase전환
+        //
+    }
+    private void ChaseUpdate()
+    {
+        //적은 범위내 상대가 없으면 무브
+        //아군 범위내 상대가 없으면 진형으로 복귀
+    }
+    private void AttackUpdate()
+    {
+        //시간재고 공격
+        //적은 범위내 상대가 없으면 무브
+        //아군 범위내 상대가 없으면 진형으로 복귀
+    }
+    void ChangeState(unitState newState)
+    {
+        switch (state)
+        {
+            case unitState.Idle:
+                break;
+            case unitState.Move:
+                break;
+            case unitState.Chase:
+                break;
+            case unitState.Attack:
+                break;
+        }
+        switch (newState)
+        {
+            case unitState.Idle:
+                break;
+            case unitState.Move:
+                break;
+            case unitState.Chase:
+                break;
+            case unitState.Attack:
+                break;
+            case unitState.Dead:
+                break;
+            default:
+                break;
+        }
+        state = newState;
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Enemy"))
+        {
+            targetTr=other.transform;
+        }
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, 10);
     }
 }
